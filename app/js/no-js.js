e46ca08514a3818f2076c9352f2062dd434e1682
ttm <taras.tataryn.ttm@gmail.com>
2018-01-22 17:16:34 +0200

    Improve no-js file

--- app/js/no-js.js
+++ app/js/no-js.js
@@ -1,5 +1,8 @@
 ;(function(){
 
+	var mainNav = document.querySelector(".main-nav--no-js");
+	mainNav.classList.remove("main-nav--no-js");
+
 	var reviews = document.querySelector(".reviews--no-js");
 	reviews.classList.remove("reviews--no-js");
 
--- app/js/script.js
+++ app/js/script.js
@@ -4,21 +4,28 @@
 	var navList = document.querySelector(".main-nav__list");
 	var navHeader = document.querySelector(".main-nav__header");
 
+	if(navToggle.classList.contains("main-nav__toggle--close")){
+		navToggle.classList.add("main-nav__toggle--open");
+		navToggle.classList.remove("main-nav__toggle--close");
+
+		navList.classList.remove("main-nav__list--display");
+		navHeader.classList.remove("main-nav__header--opened");
+	}
 
 	navToggle.addEventListener("click", function(){
-			if(navToggle.classList.contains("main-nav__toggle--open")){
-				navToggle.classList.remove("main-nav__toggle--open");
-				navToggle.classList.add("main-nav__toggle--close");
+		if(navToggle.classList.contains("main-nav__toggle--open")){
+			navToggle.classList.remove("main-nav__toggle--open");
+			navToggle.classList.add("main-nav__toggle--close");
 
-				navList.classList.add("main-nav__list--display");
-				navHeader.classList.add("main-nav__header--opened");
-			} 
-				else if(navToggle.classList.contains("main-nav__toggle--close")){
-					navToggle.classList.add("main-nav__toggle--open");
-					navToggle.classList.remove("main-nav__toggle--close");
+			navList.classList.add("main-nav__list--display");
+			navHeader.classList.add("main-nav__header--opened");
+		} 
+			else if(navToggle.classList.contains("main-nav__toggle--close")){
+				navToggle.classList.add("main-nav__toggle--open");
+				navToggle.classList.remove("main-nav__toggle--close");
 
-					navList.classList.remove("main-nav__list--display");
-					navHeader.classList.remove("main-nav__header--opened");
-			}
+				navList.classList.remove("main-nav__list--display");
+				navHeader.classList.remove("main-nav__header--opened");
+		}
 	});
 }());
