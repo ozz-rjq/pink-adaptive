9f1227fe99551d77511244c2c643621df7c21767
ttm <taras.tataryn.ttm@gmail.com>
2018-01-24 18:01:29 +0200

    Preparing to stylization photo.html

--- app/js/no-js.js
+++ app/js/no-js.js
@@ -1,9 +1,13 @@
 ;(function(){
 
 	var mainNav = document.querySelector(".main-nav--no-js");
-	mainNav.classList.remove("main-nav--no-js");
+	if(mainNav){
+		mainNav.classList.remove("main-nav--no-js");
+	}
 
 	var reviews = document.querySelector(".reviews--no-js");
-	reviews.classList.remove("reviews--no-js");
+	if(reviews){
+		reviews.classList.remove("reviews--no-js");
+	}
 
 }());
