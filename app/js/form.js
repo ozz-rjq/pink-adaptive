926a169398d241eb3a0ae78410c160d06692d644
ttm <taras.tataryn.ttm@gmail.com>
2018-02-04 11:58:47 +0200

    Modal content desktop stylization

--- app/js/form.js
+++ app/js/form.js
@@ -5,20 +5,26 @@ var submitBtn = document.querySelector(".form__submit");
 var modalBtnOk = document.querySelector(".modal__btn--ok");
 var modalBtnClose = document.querySelector(".modal__btn--close");
 
+var nameRequire = document.querySelector("input[name=name]");
+var surnameRequire = document.querySelector("input[name=surname]");
+var emailRequire = document.querySelector("input[name=email]");
+
 
 submitBtn.addEventListener("click", function(event){
 	event.preventDefault();
 
-	if(!modalSuccess.classList.contains("modal__show")){
+	if (nameRequire.value && surnameRequire && emailRequire){
 		modalSuccess.classList.add("modal__show");
+	} else if (!nameRequire.value || !surnameRequire || !emailRequire){
+		modalFailure.classList.add("modal__show");
 	}
 });
 
 modalBtnOk.addEventListener("click", function(event){
 	event.preventDefault();
 
-	if(modalSuccess.classList.contains("modal__show")){
-		modalSuccess.classList.remove("modal__show");
+	if(modalFailure.classList.contains("modal__show")){
+		modalFailure.classList.remove("modal__show");
 	}
 });
 
