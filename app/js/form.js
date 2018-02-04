4996448d23b3a11bc1a02d233cc4a5665f5d261a
ttm <taras.tataryn.ttm@gmail.com>
2018-02-04 12:07:15 +0200

    Add event on form submit

--- app/js/form.js
+++ app/js/form.js
@@ -1,6 +1,8 @@
 var form = document.querySelector(".form");
+
 var modalSuccess = document.querySelector(".modal--success");
 var modalFailure = document.querySelector(".modal--failure");
+
 var submitBtn = document.querySelector(".form__submit");
 var modalBtnOk = document.querySelector(".modal__btn--ok");
 var modalBtnClose = document.querySelector(".modal__btn--close");
@@ -13,9 +15,11 @@ var emailRequire = document.querySelector("input[name=email]");
 submitBtn.addEventListener("click", function(event){
 	event.preventDefault();
 
-	if (nameRequire.value && surnameRequire && emailRequire){
+	if (nameRequire.value && surnameRequire.value 
+			&& emailRequire.value){
 		modalSuccess.classList.add("modal__show");
-	} else if (!nameRequire.value || !surnameRequire || !emailRequire){
+	} else if (!nameRequire.value || !surnameRequire.value 
+			|| !emailRequire.value){
 		modalFailure.classList.add("modal__show");
 	}
 });
